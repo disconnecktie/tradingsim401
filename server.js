c8d15de9bedf4e463cf2c9570deceb853582adc3
disconnecktie <jay.hughes@gmail.com>
2025-03-22 16:57:48 +0100

    Add layout and CSS styling

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // server.js
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = 3000;
 
-app.set(`view engine`, `ejs`);
+// Use express-ejs-layouts
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+// Serve static files from /public
+app.use(express.static('public'));
+
+// Routes
 app.get(`/`, (req, res) => {
 	res.render(`index`, { title: `Home`, appName: `StockTrader`});
 });
 
+// Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
